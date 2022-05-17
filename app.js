97c29f7711812c2abdefcea50b104ac38073490e
Rob Scherer <robscherer6@gmail.com>
2022-05-17 14:07:28 -0400

    added clickhandler to boxes

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const table = document.getElementById('tableId');
+// const table = document.getElementById('tableId');
 
-var whosTurn = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
-var counter = 0;
+// var whosTurn = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+// var counter = 0;
 
-table.addEventListener('click', (e) => {
-  const square = e.target.closest('td');
-  if(!square.innerHTML) {
-    square.innerHTML = whosTurn[counter];
-    counter++;
-  }
-})
+// table.addEventListener('click', (e) => {
+//   const square = e.target.closest('td');
+//   if(!square.innerHTML) {
+//     square.innerHTML = whosTurn[counter];
+//     counter++;
+//   }
+// })
+
+var boxes = Array.from(document.getElementsByClassName('box'));
+
+let createBoard = () => {
+  boxes.forEach((box, index) => {
+    box.addEventListener('click', boxClicked);
+  })
+}
+
+var boxClicked = (e) => {
+  console.log('Box was clicked!!');
+}
+
+createBoard();
