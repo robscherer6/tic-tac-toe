163f57582e96825cf5db25a9d0d6733672f3126f
Rob Scherer <robscherer6@gmail.com>
2022-05-17 15:19:06 -0400

    checking for winning combinations, alerting players

--- app.js
+++ app.js
@@ -12,6 +12,13 @@
 // })
 
 var boxes = Array.from(document.getElementsByClassName('box'));
+var btn = document.getElementById('reset');
+
+btn.onclick = () => {
+  boxes.forEach((box) => {
+    box.innerHTML = '';
+  })
+}
 
 var cells = [null, null, null, null, null, null, null, null, null];
 
@@ -20,7 +27,7 @@ var player2 = 'O';
 var currentPlayer = player1;
 
 let createBoard = () => {
-  boxes.forEach((box, index) => {
+  boxes.forEach((box) => {
     box.addEventListener('click', boxClicked);
   })
 }
@@ -31,10 +38,35 @@ var boxClicked = (e) => {
   if (!cells[id]) {
     cells[id] = currentPlayer;
     e.target.innerText = currentPlayer;
+
+    if (playerWins()) {
+      alert(`${currentPlayer} wins!!!`)
+    }
     if (currentPlayer === player1) {
       currentPlayer = player2;
     } else {
       currentPlayer = player1;
+    }
+  }
+}
+
+let playerWins = () => {
+  if(cells[0] === currentPlayer) {
+    if(cells[1] === currentPlayer && cells[2] === currentPlayer) {
+      return true;
+    }
+    if(cells[3] === currentPlayer && cells[6] === currentPlayer) {
+      return true;
+    }
+    if(cells[4] === currentPlayer && cells[8] === currentPlayer) {
+      return true;
+    }
+  } else if(cells[8] === currentPlayer) {
+    if(cells[2] === currentPlayer && cells[5] === currentPlayer) {
+      return true;
+    }
+    if(cells[6] === currentPlayer && cells[7] === currentPlayer) {
+      return true;
     }
   }
 }
