a3d20ade9d6ba6fee11f4e17f4c2b28409d77f99
Rob Scherer <robscherer6@gmail.com>
2022-05-16 22:59:30 -0400

    adding clickHandler to table cell

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello World!')
+function addRowHandlers() {
+  var table = document.getElementById('tableId');
+  var rows = table.getElementsByTagName('tr');
+  for (var i = 0; i < rows.length; i++) {
+    var currentRow = rows[i];
+    var createClickHandler = function (row) {
+      return function () {
+        var cell = row.getElementsByTagName('td')[0];
+        var id = cell.innerHTML;
+        id.innerText = 'X'
+      }
+    }
+    currentRow.onclick = createClickHandler(currentRow);
+  }
+}
+
+window.onload = addRowHandlers();
